9f7bbdc50fb0fff6d6e6b6bee57ef28342079c2c
erhanturan1991 <62485957+erhanturan1991@users.noreply.github.com>
2020-04-03 01:22:37 +0300

    Update ysaiteratif.java

--- ysaiteratif.java
+++ ysaiteratif.java
@@ -1,38 +1,35 @@
 package crossValidation;
 
+/* In this block of code, testing was performed on the marble data set of the Artifical Neural Network
+ * classifier known as MLP in the weka library. The MLP classification success of the momentum coefficient
+ * was tested using 10-fold cross-validation. Testing is done by accepting the number of standard cycles as
+ * 500 and the learning rate as 0.6.
+ * */
+
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.Utils;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.MultilayerPerceptron;
-import weka.classifiers.functions.SMO;
-import weka.classifiers.functions.supportVector.Kernel;
-import weka.classifiers.trees.J48;
-import gui.ELMM;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Random;
 
-
 public class  ysaiteratif {
-
 
 	public static void main(String[] args) throws Exception {
 		try {	
-			Instances train = new Instances(new BufferedReader(new FileReader("C:\\Users\\Erhan\\eclipse-workspace\\mermer_kalitelendirme\\src\\crossValidation\\mix4sHistLBP.arff")));
+			Instances train = new Instances(new BufferedReader(new FileReader("path\\mix4sHistLBP.arff")));
 			int lastIndex = train.numAttributes() - 1;				            
 			train.setClassIndex(lastIndex);			
 			MultilayerPerceptron mlp=new MultilayerPerceptron();
-			//mlp.setMomentum(0.6);				
-			//mlp.setLearningRate(0.9);
-			mlp.setHiddenLayers("a");
+			
+			mlp.setHiddenLayers("a");//Automatic neuraon count a=(attributes+classes)/2
 			mlp.setNumDecimalPlaces(2);
 			mlp.setTrainingTime(500);
 			mlp.setDoNotCheckCapabilities(true);		     
-			mlp.setDecay(false);
-			//mlp.setGUI(true);	
+			mlp.setDecay(false);		
 			String basari= "";
 			for(int i=0;i<7;i++){
 				Double momentum=0.2+(0.1*i);
@@ -42,7 +39,7 @@ public class  ysaiteratif {
 					mlp.setLearningRate(learning);
 					mlp.buildClassifier(train);	
 					mlp.getAutoBuild();
-					//System.out.println("YSA Success Metrics: ");
+					//System.out.println("MLP Success Metrics: ");
 					Evaluation eval = new Evaluation(train);		            
 					//eval.evaluateModel(mlp, train);
 					eval.crossValidateModel(mlp, train, 10, new Random(1));
